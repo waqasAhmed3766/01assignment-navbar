1de734fe408c81db0b924f4bda12a88b93331ba5
waqaskhansahb <waqasahmed3766@gmail.com>
2024-11-01 01:44:31 +0500

    change

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function Contact() {
   return (
     <div className='bg-red-200 h-[400px] w-full'>
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-
-export default function Main() {
-  return (
-    <div></div>
-  )
-} 
