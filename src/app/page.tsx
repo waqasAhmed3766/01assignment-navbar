0e79ed887aa15fb7aa87dc630f7e136d1b9fc25c
waqaskhansahb <waqasahmed3766@gmail.com>
2024-11-03 22:54:04 +0500

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,8 @@ export default function Main() {
            <h1 className="h-20 flex justify-center items-center text-6xl bg-blue-500 text-white font-serif">**********Home Page**********</h1>  
            <div className="font-semibold flex justify-center items-center bg-yellow-300 pt-32 pb-32 text-2xl">
                  
-          <span></span>A web developer designs and builds websites. They create pages, add content, and ensure the website looks great and works well. Web developers can specialize in front-end (what users see) or back-end (how it works behind the scenes). A full-stack developer does both! They handle both the front-end, like visuals and user experience, and the back-end, which involves server and database management. Full-stack developers have a wide range of skills, from HTML, CSS, and JavaScript, to databases and server-side languages. They make websites come alive, ensuring they’re fast, functional, and user-friendly. This versatile role is highly valued in tech, as it combines creativity with technical knowledge.
+          <p className="pl-40 pr-40 text-gray-50">
+             A web developer designs and builds websites. They create pages, add content, and ensure the website looks great and works well. Web developers can specialize in front-end (what users see) or back-end (how it works behind the scenes). A full-stack developer does both! They handle both the front-end, like visuals and user experience, and the back-end, which involves server and database management. Full-stack developers have a wide range of skills, from HTML, CSS, and JavaScript, to databases and server-side languages. They make websites come alive, ensuring they’re fast, functional, and user-friendly. This versatile role is highly valued in tech, as it combines creativity with technical knowledge.</p>
             </div> 
     </div>
   )
