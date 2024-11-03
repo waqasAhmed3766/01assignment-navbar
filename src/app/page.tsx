35ddb667df0f0be24bb6ae8d069fcb671d17af72
waqaskhansahb <waqasahmed3766@gmail.com>
2024-11-03 21:13:11 +0500

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import Navbar from "./components/navbar";
 export default function Main() {
   return (
     <div>
-           <h1>Hello</h1>   
+           <h1 className="h-20 flex justify-center items-center text-6xl bg-blue-500 text-white font-serif">**********Home Page**********</h1>  
+           <div className="font-semibold flex justify-center items-center bg-yellow-300 pt-32 pb-32 text-2xl">
+                 
+          <span></span>A web developer designs and builds websites. They create pages, add content, and ensure the website looks great and works well. Web developers can specialize in front-end (what users see) or back-end (how it works behind the scenes). A full-stack developer does both! They handle both the front-end, like visuals and user experience, and the back-end, which involves server and database management. Full-stack developers have a wide range of skills, from HTML, CSS, and JavaScript, to databases and server-side languages. They make websites come alive, ensuring they’re fast, functional, and user-friendly. This versatile role is highly valued in tech, as it combines creativity with technical knowledge.
+            </div> 
     </div>
   )
 } 
